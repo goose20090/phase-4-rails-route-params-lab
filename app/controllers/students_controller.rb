adb32c370848613fc5fb9390cbbc10e0afc37b4e
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-04-04 14:35:16 +0100

    commit tree 94e0c77fa8e360af23b08c5bcc957cde2db42f56

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,25 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if student_params[:name]
+      students = Student.where("first_name LIKE ? OR last_name LIKE ?", student_params[:name], student_params[:name])
+    end
     render json: students
+  end
+
+  def show
+    student = find_student
+    render json: student
+  end
+
+  private 
+
+  def find_student
+    Student.find(params[:id])
+  end
+  
+  def student_params
+    params.permit(:id, :name)
   end
 
 end
